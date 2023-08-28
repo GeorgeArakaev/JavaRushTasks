06e91848e68480c8e0ae72997d339019805fb4ef
Home <arakaev2012@gmail.com>
2023-08-28 19:35:33 +0500

    Add solution java syntax task 0103 It's 3126 now. I decided in what year my friend was born, if he is now 8 years old.

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3126;
+        int b = 8;
+        int x = a - b;
+                System.out.println (x);
+}
+}
