817cf0a8dbfd93ddc7ce71b265e9cccdf764c77f
Home <arakaev2012@gmail.com>
2023-09-09 21:50:42 +0500

    Add solution java syntax task 0305

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -8,6 +8,19 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+        if (number1 == number2 && number3 == number2) {
+            System.out.println(number1 + " " + number2 + " " + number3);
+        } else if (number1 == number2) {
+            System.out.println(number1 + " " + number2);
 
+        } else if (number2 == number3) {
+            System.out.println(number2 + " " + number3);
+        } else if (number3 == number1) {
+            System.out.println(number3 + " " + number1);
+        }
     }
 }
