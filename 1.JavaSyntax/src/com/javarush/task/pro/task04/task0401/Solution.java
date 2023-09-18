503f784c496bb793013a859f723c3f258f65cc06
Home <arakaev2012@gmail.com>
2023-09-18 13:23:41 +0500

    Add solution java syntax task 0401

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0401/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task04.task0401;
+
+/* 
+Я никогда не буду работать за копейки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String quote = "Я никогда не буду работать за копейки. Амиго";
+        int u = 100;
+        int a = 0;
+        while (a<u)
+        {
+            a++;
+            System.out.println(quote);
+        }
+    }
+}
